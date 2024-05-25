f2890f6018747f86ca8a4c2053806d9bd30b4cdd
VaishnavAkash <akashgupta9062@gmail.com>
2024-05-25 15:15:28 +0530

    update : finished

--- app/page.js
+++ app/page.js
@@ -1,13 +1,21 @@
 "use client";
 
+import { useEffect } from "react";
 import Home from "@/components/Home.jsx";
 import SignIn from "@/components/SignIn.jsx";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
   const router = useRouter();
-  const isAuthorized =
-    localStorage.getItem("username") && localStorage.getItem("password");
 
-  return isAuthorized ? <Home /> : router.push("/sign-in");
+  useEffect(() => {
+    const isAuthorized =
+      localStorage.getItem("username") && localStorage.getItem("password");
+
+    if (!isAuthorized) {
+      router.push("/sign-in");
+    }
+  }, []);
+
+  return <Home />;
 }
